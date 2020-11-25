ca85ec973b88ac102b56d9ff328837551edcfc8e
Martin Simecek <msimecek@users.noreply.github.com>
2020-11-25 20:00:32 +0100

    Connect script, no credentials

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = '<yourservername>.mariadb.database.azure.com';
+$username = '<yourusername>@<yourservername>';
+$password = '<yourpassword>';
+$db_name = 'testdb';
+
+echo "mysqlnd_azure.enableRedirect: ", ini_get("mysqlnd_azure.enableRedirect"), "\n";
+$db = mysqli_init();
+
+//The connection must be configured with SSL for redirection test
+$link = mysqli_real_connect ($db, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+if (!$link) {
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+}
+else {
+    echo $db->host_info, "\n"; //if redirection succeeds, the host_info will differ from the hostname you used used to connect
+    $res = $db->query('SHOW TABLES;'); //test query with the connection
+    print_r ($res);
+    $db->close();
+}
+
+?>
